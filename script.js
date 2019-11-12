14f3e4b96b05bb1ac7be3c2b9e6292f40af76723
Tiffany Wong <tif.wong@yahoo.com>
2019-11-11 20:59:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// deck of cards (random)
+const cardArray = new Array();
+
+
+// shuffle deck of cards
+
+// dealer and player1 cards
+
+// add cards
+
+// if dealers hit 17 no need cards, but lower than 17 get more cards
+
+// player1 (hit, stand, new game)
+
+
+
+
+// init
+card.init = function () {
+
+}
+
+// document ready
+$(function () {
+    card.init();
+    console.log("ready")
+});
+
+// STRETCH GOALS
+// add how many wins, loses and draws
+// betting
+// more deck of cards
+// add more players to the game
