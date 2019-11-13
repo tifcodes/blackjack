5d86631dc47e88abbb972adb63950191b398a595
Tiffany Wong <tif.wong@yahoo.com>
2019-11-13 11:18:06 -0500

    show cards

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// deck of cards (random)
+// deck of cards 
 const card = {};
 const cardSuits = ["diamonds", "hearts", "spades", "clubs"];
 const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 let cardArray = new Array();
 
-card.getCard = function() {
+card.getCard = function () {
     let cardArray = new Array();
 
     for (let s = 0; s < cardSuits.length; s++) {
 
-        for (let v = 0; v < cardValues.length; v++){
+        for (let v = 0; v < cardValues.length; v++) {
 
-            let card = {Value: cardValues[v], Suit: cardSuits[s]};
+            let card = { Value: cardValues[v], Suit: cardSuits[s] };
             cardArray.push(card);
         }
     }
@@ -19,7 +19,7 @@ card.getCard = function() {
 }
 
 // shuffle deck of cards
-card.shuffleCards = function() {
+card.shuffleCards = function () {
     for (let i = 0; i < 500; i++) {
         let cardPlace1 = Math.floor(Math.random() * cardArray.length);
         let cardPlace2 = Math.floor(Math.random() * cardArray.length);
@@ -29,14 +29,61 @@ card.shuffleCards = function() {
 }
 
 // deal card
-card.dealCards = function(){
-    console.log(cardArray.pop())
+card.dealCards = function () {
     return cardArray.pop();
 }
 
-// dealer and player1 cards
+// dealer and player1 cards (show cards)
+card.showCards = function () {
+    let cardValueArray = [];
+    for (let i = 0; i < 4; i++) {
+        let cardValue = card.dealCards().Value;
+        cardValueArray.push(cardValue);
+    }
+    return cardValueArray;
+}
+card.showEachCards = function() {
+    $(".playerCard1").append(card.showCards()[0]);
+    $(".dealerCard1").append(card.showCards()[1]);
+    $(".playerCard2").append(card.showCards()[2]);
+    $(".dealerCard2").append(card.showCards()[3]);
+}
 
-// add cards
+    // return cardValue
+    // console.log(cardValueArray.push("heloo"))
+    // $(".playerCard1").append(cardValue);
+    // $(".dealerCard1").append(cardValue);
+    // $(".playerCard1").append(cardValue);
+    // let cardValue = card.dealCards().Value;
+    // let suitValues = card.dealCards().Suit;
+    // $(".playerCard1").append(card.dealCards().Value);
+    // $(".dealerCard1").append(card.dealCards().Value);
+    // $(".playerCard2").append(card.dealCards().Value);
+    // $(".dealerCard2").append(card.dealCards().Value);
+    //     if (suitValue = "diamonds") {
+    //         $(".playerCard1").append(`${cardValue} ♦️ ${cardValue}`);
+    //         $(".dealerCard1").append(`${cardValue} ♦️ ${cardValue}`);
+    //         $(".playerCard2").append(`${cardValue} ♦️ ${cardValue}`);
+    //         $(".dealerCard2").append(`${cardValue} ♦️ ${cardValue}`);
+    //     } else if (suitValue = "hearts") {
+    //         $(".playerCard1").append(`${cardValue} ♥️ ${cardValue}`);
+    //         $(".dealerCard1").append(`${cardValue} ♥️ ${cardValue}`);
+    //         $(".playerCard2").append(`${cardValue} ♥️ ${cardValue}`);
+    //         $(".dealerCard2").append(`${cardValue} ♥️ ${cardValue}`);
+    //     } else if (suitValue = "spades") {
+    //         $(".playerCard1").append(`${cardValue} ♠️ ${cardValue}`);
+    //         $(".dealerCard1").append(`${cardValue} ♠️ ${cardValue}`);
+    //         $(".playerCard2").append(`${cardValue} ♠️ ${cardValue}`);
+    //         $(".dealerCard2").append(`${cardValue} ♠️ ${cardValue}`);
+    //     } else if (suitValue = "clubs") {
+    //         $(".playerCard1").append(`${cardValue} ♣️ ${cardValue}`);
+    //         $(".dealerCard1").append(`${cardValue} ♣️ ${cardValue}`);
+    //         $(".playerCard2").append(`${cardValue} ♣️ ${cardValue}`);
+    //         $(".dealerCard2").append(`${cardValue} ♣️ ${cardValue}`);
+    // }}
+
+
+// add cards (banker and player)
 
 
 // if dealers hit 17 no need cards, but lower than 17 get more cards
@@ -47,7 +94,7 @@ card.dealCards = function(){
 card.init = function () {
     cardArray = card.getCard()
     card.shuffleCards();
-    card.dealCards();
+    card.showEachCards();
 }
 
 // document ready
@@ -60,3 +107,5 @@ $(function () {
 // betting
 // more deck of cards
 // add more players to the game
+// insurance
+// split
