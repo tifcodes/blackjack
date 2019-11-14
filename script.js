44ce9d07840e1e12e5fa341de3f57e82a2497ccd
Tiffany Wong <tif.wong@yahoo.com>
2019-11-13 20:36:37 -0500

    suits

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // deck of cards 
 const card = {};
-const cardSuits = ["diamonds", "hearts", "spades", "clubs"];
+const cardSuits = ["♦️", "♥️", "♠️", "♣️"];
 const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 let cardArray = new Array();
 
@@ -38,52 +38,30 @@ card.showCards = function () {
     let cardValueArray = [];
     for (let i = 0; i < 4; i++) {
         let cardValue = card.dealCards().Value;
-        cardValueArray.push(cardValue);
+        let suitValue = card.dealCards().Suit;
+        cardValueArray.push(cardValue, suitValue);
     }
+    console.log(cardValueArray)
     return cardValueArray;
 }
-card.showEachCards = function() {
-    $(".playerCard1").append(card.showCards()[0]);
-    $(".dealerCard1").append(card.showCards()[1]);
-    $(".playerCard2").append(card.showCards()[2]);
-    $(".dealerCard2").append(card.showCards()[3]);
+
+card.showEachCards = function () {
+    const showEachCardArray = card.showCards();
+    $(".playerCard1").append(`<p>${showEachCardArray[0]}</p> <p class = "suits">${showEachCardArray[1]}</p> <p class = "value"> ${showEachCardArray[0]}</p>`);
+    $(".dealerCard1").append(`<p>${showEachCardArray[2]}</p> <p class = "suits">${showEachCardArray[3]}</p> <p class = "value"> ${showEachCardArray[2]}</p>`);
+    $(".playerCard2").append(`<p>${showEachCardArray[4]}</p> <p class = "suits">${showEachCardArray[5]}</p> <p class = "value"> ${showEachCardArray[4]}</p>`);
+    $(".dealerCard2").append(`<p>${showEachCardArray[6]}</p> <p class = "suits">${showEachCardArray[7]}</p> <p class = "value"> ${showEachCardArray[6]}</>`);
 }
 
-    // return cardValue
-    // console.log(cardValueArray.push("heloo"))
-    // $(".playerCard1").append(cardValue);
-    // $(".dealerCard1").append(cardValue);
-    // $(".playerCard1").append(cardValue);
-    // let cardValue = card.dealCards().Value;
-    // let suitValues = card.dealCards().Suit;
-    // $(".playerCard1").append(card.dealCards().Value);
-    // $(".dealerCard1").append(card.dealCards().Value);
-    // $(".playerCard2").append(card.dealCards().Value);
-    // $(".dealerCard2").append(card.dealCards().Value);
-    //     if (suitValue = "diamonds") {
-    //         $(".playerCard1").append(`${cardValue} ♦️ ${cardValue}`);
-    //         $(".dealerCard1").append(`${cardValue} ♦️ ${cardValue}`);
-    //         $(".playerCard2").append(`${cardValue} ♦️ ${cardValue}`);
-    //         $(".dealerCard2").append(`${cardValue} ♦️ ${cardValue}`);
-    //     } else if (suitValue = "hearts") {
-    //         $(".playerCard1").append(`${cardValue} ♥️ ${cardValue}`);
-    //         $(".dealerCard1").append(`${cardValue} ♥️ ${cardValue}`);
-    //         $(".playerCard2").append(`${cardValue} ♥️ ${cardValue}`);
-    //         $(".dealerCard2").append(`${cardValue} ♥️ ${cardValue}`);
-    //     } else if (suitValue = "spades") {
-    //         $(".playerCard1").append(`${cardValue} ♠️ ${cardValue}`);
-    //         $(".dealerCard1").append(`${cardValue} ♠️ ${cardValue}`);
-    //         $(".playerCard2").append(`${cardValue} ♠️ ${cardValue}`);
-    //         $(".dealerCard2").append(`${cardValue} ♠️ ${cardValue}`);
-    //     } else if (suitValue = "clubs") {
-    //         $(".playerCard1").append(`${cardValue} ♣️ ${cardValue}`);
-    //         $(".dealerCard1").append(`${cardValue} ♣️ ${cardValue}`);
-    //         $(".playerCard2").append(`${cardValue} ♣️ ${cardValue}`);
-    //         $(".dealerCard2").append(`${cardValue} ♣️ ${cardValue}`);
-    // }}
-
-
 // add cards (banker and player)
+card.addCards = function(){
+    const showEachCardArray = card.showCards();
+    showEachCardArray.filter(function(number) {
+        console.log(parseInt(number))
+        return parseInt(number)
+    })
+    console.log(showEachCardArray)
+}
 
 
 // if dealers hit 17 no need cards, but lower than 17 get more cards
@@ -94,7 +72,9 @@ card.showEachCards = function() {
 card.init = function () {
     cardArray = card.getCard()
     card.shuffleCards();
+    card.showCards();
     card.showEachCards();
+    card.addCards();
 }
 
 // document ready
